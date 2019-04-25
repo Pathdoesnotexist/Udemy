1a67d75422b9ef1e24059211899896b99f688788
Nulnoil <nulnoil@outlook.com>
2019-04-25 18:58:59 +0300

    Первый коммит Юдеми

---
+++ lessonsScript.js
@@ -0,0 +1,19 @@
+'use strict';
+// console.log(fst);
+// var fst = "внешний вар ";
+// console.log(snd);
+
+// if (fst) {
+//   var snd = " внутренний вар";
+//   console.log(fst);
+//   console.log(snd);
+// }
+// console.log(fst + snd);
+
+// alert('test2');
+
+// let answer = confirm('Are youy here?');
+// console.log(answer);
+
+let answer = prompt('Are you over 18?', 'yes');
+console.log(answer);
