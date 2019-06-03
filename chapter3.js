3afbe9b2c506e2bfb5b63d813c5353a8790eda27
Nulnoil <nulnoil@outlook.com>
2019-06-03 18:15:42 +0300

    задание 12 выполнено

--- chapter3.js
+++ chapter3.js
@@ -3,6 +3,108 @@ alert('Lesson 3-27');
 
 
 
+
+
+let video = ['youtube', 'vimeo', 'rutube'];
+let blogs = ['wordpress', 'lj', 'blogger'];
+let internet = [...video, ...blogs, 'vk', 'facebooks'];
+
+console.log(internet);
+
+function log (a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+let numbers = [2, 5, 7];
+
+log(...numbers);
+
+
+
+//3.31////////////////////////////////
+// class Rectangle {
+//     constructor(height, width = 25, lenght = 3) {
+//         this.height = height;
+//         this.width = width;
+//         this.lenght = lenght;
+//     }
+//     calcArea() {
+//         return this.height * this.width * this.lenght;
+//     }
+// }
+
+// const square = new Rectangle (10);
+
+// console.log(square.calcArea());
+
+
+//3.30////////////////////////////////
+
+// function calcOrDouble (number, basis = 2) {
+//     // basis = basis || 2;  //ES 5
+//     console.log(number * basis);
+// }
+// calcOrDouble(3, 5);
+// calcOrDouble(6);
+
+//3.29////////////////////////////////
+// let fun = () => {
+//     console.log(this);
+// };
+// fun();
+
+// let obj = {
+//     number: 5,
+//     sayNumber: function (){
+//         let say = () => {
+//             console.log(this);
+//         };
+//         say();
+//     }
+// };
+// obj.sayNumber();
+
+
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(){
+//     let show = () => {
+//         console.log(this);
+//     };
+// show();
+// });
+
+
+//3.28////////////////////////////////
+
+// function makeArray() {
+//     var items = [];
+
+//     for(let i = 0; i < 10;i++) { //for(var i = 0; i < 10;i++) {
+//         var item = function() {
+//             console.log(i);
+//         };
+//         items.push(item);
+//     }
+//     return items;
+// }
+
+// var arr = makeArray();
+
+// arr[1]();
+// arr[3]();
+// arr[7]();
+
+///////////////////////////////////3.27////////////////////////////////////////////////////
+// let name = 'Ivan',
+//     age = 30,
+//     mail = 'ex@mail.ru';
+
+// document.write('Пользователю ' + name + ' ' + age + ' лет. Его почта ' + mail);
+// document.write(`Пользователю ${name} ${age} лет. Его почта ${mail}`);
+
+
 ////////////////////////////////ЗАДАНИЕ 11 ЧАСТЬ 2////////////////////////////////////////////////////////
 // let age = document.getElementById('age');
 // function showUser(surname, name) {
